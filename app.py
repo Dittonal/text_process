081ac0ad9055052b8c9f842a3c09b4a96028fa04
X <53260392+Dittonal@users.noreply.github.com>
2025-02-07 21:33:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ en_stopwords = requests.get(en_stopwords_url).text.splitlines()
 # Function to clean text
 def clean_text(text, language):
     text = text.replace('\n', ' ').replace('\r', ' ').strip()
+    URL_REGEX = re.compile(
+            r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',
+            re.IGNORECASE)
+    text = re.sub(URL_REGEX, "", text)
     if language == "EN":
         text = re.sub(r'[^A-Za-z0-9\s]', ' ', text)
         text = text.lower()
@@ -36,18 +40,11 @@ def clean_text(text, language):
         text = re.sub(r"(回复)?(//)?\s*@\S*?\s*(:| |$)", " ", text)  # 去除正文中的@和回复/转发中的用户名
         text = re.sub(r"\[\S+\]", "", text)      # 去除表情符号
         # text = re.sub(r"#\S+#", "", text)      # 保留话题内容
-        URL_REGEX = re.compile(
-            r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',
-            re.IGNORECASE)
-        text = re.sub(URL_REGEX, "", text)       # 去除网址
         text = text.replace("转发微博", "")       # 去除无意义的词语
         text = re.sub(r"\s+", " ", text) # 合并正文中过多的空格
         text = re.sub(r'[^\u4e00-\u9fff]+', ' ',text)
     else:
-        URL_REGEX = re.compile(
-            r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',
-            re.IGNORECASE)
-        text = re.sub(URL_REGEX, "", text)
+        text=text
     return text.strip()
 
 # Function to perform word segmentation and POS tagging
