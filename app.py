0fc3328b72d6f609d017c7741dbe83a520d870c4
X <53260392+Dittonal@users.noreply.github.com>
2025-02-07 12:54:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 import requests
 from io import BytesIO
+import re
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
@@ -23,7 +24,18 @@ def clean_text(text, language):
     text = text.replace('\n', ' ').replace('\r', ' ').strip()
     if language == "英文":
         text = text.lower()
-    return text
+    else:
+        text = re.sub(r"(回复)?(//)?\s*@\S*?\s*(:| |$)", " ", text)  # 去除正文中的@和回复/转发中的用户名
+        text = re.sub(r"\[\S+\]", "", text)      # 去除表情符号
+        # text = re.sub(r"#\S+#", "", text)      # 保留话题内容
+        URL_REGEX = re.compile(
+            r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))',
+            re.IGNORECASE)
+        text = re.sub(URL_REGEX, "", text)       # 去除网址
+        text = text.replace("转发微博", "")       # 去除无意义的词语
+        text = re.sub(r"\s+", " ", text) # 合并正文中过多的空格
+        text = re.sub(r'[^\u4e00-\u9fff]+', ' ',text)
+    return text.strip()
 
 # Function to perform word segmentation and POS tagging
 def segment_and_tag(text, language):
