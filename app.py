1ce4aa116f74a9c55bd81f87b76318e6affad5fe
X <53260392+Dittonal@users.noreply.github.com>
2025-02-08 08:47:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import re
 import kiwipiepy
 from kiwipiepy import Kiwi
 from kiwipiepy.utils import Stopwords
+from docx import Document  
 stopwords = Stopwords()
 nltk.download('punkt')
 nltk.download('punkt_tab')
